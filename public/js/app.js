58a75235e380b9f08082126365a3382e49604631
Scott Hummel <scotthummel@icloud.com>
2017-10-05 22:39:59 -0700

    Another test

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,34 @@
-"use strict";function _interopRequireDefault(e){return e&&e.__esModule?e:{default:e}}require("./bootstrap");var _routes=require("./routes"),_routes2=_interopRequireDefault(_routes);Vue.component("psg-header",require("./components/Header")),Vue.component("psg-page",require("./components/Page")),Vue.component("psg-footer",require("./components/Footer")),Vue.mixin({methods:{charge:function(e){var t=document.querySelector(e),r=document.head.querySelector('meta[name="csrf-token"]'),o=document.createElement("input");o.type="hidden",o.name="_token",o.value=r.content,t.append(o)}}});var app=new Vue({el:"#app",router:_routes2.default});
+'use strict';
+
+require('./bootstrap');
+
+var _routes = require('./routes');
+
+var _routes2 = _interopRequireDefault(_routes);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+Vue.component('psg-header', require('./components/Header'));
+Vue.component('psg-page', require('./components/Page'));
+Vue.component('psg-footer', require('./components/Footer'));
+
+Vue.mixin({
+    methods: {
+        charge: function charge(formId) {
+            var form = document.querySelector(formId);
+
+            var token = document.head.querySelector('meta[name="csrf-token"]');
+            var input = document.createElement('input');
+            input.type = 'hidden';
+            input.name = '_token';
+            input.value = token.content;
+
+            form.append(input);
+        }
+    }
+});
+
+var app = new Vue({
+    el: '#app',
+    router: _routes2.default
+});
