3bdb5a68cac5ec08b583d99d5478de4de01fa40f
Scott Hummel <scotthummel@icloud.com>
2017-10-05 22:59:34 -0700

    Yet another try

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,7 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.babel('resources/assets/js/app.js', 'public/js/app.js')
+mix.js('resources/assets/js/app.js', 'public/js/app.js')
    .sass('resources/assets/sass/app.scss', 'public/css')
     .browserSync('shanti.dev');
 
