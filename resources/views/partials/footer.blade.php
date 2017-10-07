232802009918355be80a45bdff0c66e72277917b
Scott Hummel <scotthummel@icloud.com>
2017-10-07 08:46:09 -0700

    main.js

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -29,6 +29,6 @@
 
 {{--<script src="/js/manifest.js"></script>--}}
 {{--<script src="/js/vendor.js"></script>--}}
-<script src="{{ mix('/js/app.js') }}"></script>
+<script src="{{ mix('/js/main.js') }}"></script>
 </body>
 </html>
