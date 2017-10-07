4d7aee729c7ff86850d43ec2b3da4b41ade94ecb
Scott Hummel <scotthummel@icloud.com>
2017-10-06 21:00:00 -0700

    Shims

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -29,6 +29,13 @@
 
 {{--<script src="/js/manifest.js"></script>--}}
 {{--<script src="/js/vendor.js"></script>--}}
+<script src="https://cdnjs.cloudflare.com/ajax/libs/es5-shim/4.5.7/es5-shim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/es5-shim/4.5.7/es5-sham.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/json3/3.3.2/json3.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/es6-shim/0.34.2/es6-shim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/es6-shim/0.34.2/es6-sham.min.js"></script>
+<script src="https://wzrd.in/standalone/es7-shim@latest"></script>
+<script src="other-libs.js"></script>
 <script src="{{ mix('/js/app.js') }}"></script>
 </body>
 </html>
